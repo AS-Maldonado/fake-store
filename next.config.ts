c6e553c54585067876cde782c818138939dacf29
Augusto <asmaldonado02@gmail.com>
2025-04-23 23:36:57 -0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  transpilePackages: ["@storefront-ui/react"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        port: "",
+        pathname: "/sfui_docs_artifacts_bucket_public/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
